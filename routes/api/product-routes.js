87b45a60b888bb27136fc3c41b5ec95474bae686
James Kelly <jk377y@gmail.com>
2023-01-29 11:44:46 -0600

    final touches on comments

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,30 +4,30 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 //! get all products
-router.get('/', async (req, res) => {
-  // find all products
-  try {
-    const productData = await Product.findAll({
-      // be sure to include its associated Category and Tag data
-      include: [{model: Category}, {model: Tag,}],
+//! find all products
+//! be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {  // router is an instance of Express Router, "get" means it is retreiving data, "/" is the endpoint, this will be an async function
+  try {  // try block should handle any errors that occur within this block of code
+    const productData = await Product.findAll({  // declares a constant variable named productData and assigns the result of calling the Product.findAll() method to it
+      include: [{model: Category}, {model: Tag,}],  // specifies that the Category and Tag data should be included in the retrieval
     });
-    res.status(200).json(productData);  // sends a JSON response to the client with a status code of 200 and the data stored in the productData constant. The json method is used to convert the productData object to JSON format and set the response content type to application/json
-  } catch (err) {
+    res.status(200).json(productData);  // json(productData) method is then used to send a JSON response to the client, with the retrieved data productData as the response body
+  } catch (err) {  // if an error occurs above, this will store the error in a variable named err
     console.error(err);  // log the error message
     res.status(500).json({error: err.message});  // return the error message in JSON format
   }
 });
 
 //! get one product
-router.get('/:id', async (req, res) => {
-  // find a single product by its `id`
-  try {
-    const productData = await Product.findByPk(req.params.id, {
-      // be sure to include its associated Category and Tag data
-      include: [{model: Category}, {model: Tag}],
+//! find a single product by its `id`
+//! be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {  // router is an instance of Express Router, "get" means it is retreiving data, "/:id" is requesting a specific item in the endpoint, this will be an async function
+  try {  // try block should handle any errors that occur within this block of code
+    const productData = await Product.findByPk(req.params.id, {  // retrieves the product data from the database using the Sequelize findByPk method (primary key)
+      include: [{model: Category}, {model: Tag}],  // specifies that the Category and Tag data should be included in the retrieval
     });
     res.status(200).json(productData);  // sends a JSON response to the client with a status code of 200 and the data stored in the productData constant. The json method is used to convert the productData object to JSON format and set the response content type to application/json
-  } catch (err) {
+  } catch (err) {  // if an error occurs above, this will store the error in a variable named err
     console.error(err);  // log the error message
     res.status(500).json({error: err.message});  // return the error message in JSON format
   }
@@ -107,16 +107,16 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', async (req, res) => {
-  //! delete one product by its `id` value
+//! delete one product by its `id` value
+router.delete('/:id', async (req, res) => {  // router is an instance of Express Router, "delete" means it is deleting existing data, "/:id" is the endpoint that is specifying which item to delete, this will be an async function
   try {  // try block handles any potential errors during the execution of the code
     const productData = await Product.destroy({  // destroy() deletes a specific Product identified by its primary key value
       where: {  // where option is used to specify which product should be deleted based on its id
-        id: req.params.id,  // which is passed in the URL as a parameter
+        id: req.params.id,  // this is the id that will be deleted, which is passed from the URL as a parameter
       },
     });
     res.status(200).json(productData);  // sends a JSON response to the client with a status code of 200 and the data stored in the productData constant. The json method is used to convert the productData object to JSON format and set the response content type to application/json
-  } catch (err) {
+  } catch (err) {  // try block should handle any errors that occur within this block of code
     console.error(err);  // log the error message
     res.status(500).json({error: err.message});  // return the error message in JSON format
   }
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,44 +4,44 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 //! GET route to retreive ALL tags from db
-router.get('/', async (req, res) => {
+router.get('/', async (req, res) => {  // router is an instance of Express Router, "get" means it is retreiving data, "/" is the endpoint, this will be an async function
   try {  // try block handles any potential errors during the execution of the code
     const tagData = await Tag.findAll({  // findAll() provided used to retrieve all tags from the database
       include: [{model: Product,}],   // include option is used to specify that associated Product data should be included in the query results
     });
     res.status(200).json(tagData);  // returns json data if successful
-  } catch (err) {
+  } catch (err) {  // if an error occurs above, this will store the error in a variable named err
     console.error(err);  // log the error message
     res.status(500).json({error: err.message});  // return the error message in JSON format
   }
 });
 
 //! GET route to retreive a single tag from the db
-router.get('/:id', async (req, res) => {
+router.get('/:id', async (req, res) => {  // router is an instance of Express Router, "get" means it is retreiving data, "/:id" is requesting a specific item in the endpoint, this will be an async function
 try {  // try block handles any potential errors during the execution of the code
   const tagData = await Tag.findByPk(req.params.id, {  // findByPk() provided used to retrieve a single tag from the database
     include: [{model: Product}],   // include option is used to specify that associated Product data should be included in the query results
   });
   res.status(200).json(tagData);  // returns json data if successful
-} catch (err) {
+} catch (err) {  // if an error occurs above, this will store the error in a variable named err
   console.error(err);  // log the error message
   res.status(500).json({error: err.message});  // return the error message in JSON format
 }
 });
 
 //! POST route to create a new tag in the db
-router.post('/', async (req, res) => {
+router.post('/', async (req, res) => {  // router is an instance of Express Router, "post" means it is adding new data, "/" is the endpoint, this will be an async function
   try {  // try block handles any potential errors during the execution of the code
     const tagData = await Tag.create(req.body)  // uses create() to create a new tag in the db; req.body is used to access the data sent in the request body
     res.status(200).json(tagData)  // returns json data if successful
-  } catch (err) {
+  } catch (err) {  // if an error occurs above, this will store the error in a variable named err
     console.error(err);  // log the error message
     res.status(500).json({error: err.message});  // return the error message in JSON format
   }
 });
 
 //! PUT route to update a specific tag in the db
-router.put('/:id', async (req, res) => {
+router.put('/:id', async (req, res) => {  // router is an instance of Express Router, "put" means it is updating existing data, "/:id" is the endpoint that is specifying which item to update, this will be an async function
   try {  // try block handles any potential errors during the execution of the code
     const tagData = await Tag.update(req.body, {  // uses update() to update a specific tag; req.body is used to access the data sent in the request body
       where: {  // where option is used to specify which tag should be updated based on its id
@@ -49,14 +49,14 @@ router.put('/:id', async (req, res) => {
       },
     });
     res.status(200).json(tagData);  // returns json data if successful
-  } catch (err) {
+  } catch (err) {  // if an error occurs above, this will store the error in a variable named err
     console.error(err);  // log the error message
     res.status(500).json({error: err.message});  // return the error message in JSON format
   }
 });
 
 //! DELETE route to delete a specific id
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', async (req, res) => {  // router is an instance of Express Router, "delete" means it is deleting existing data, "/:id" is the endpoint that is specifying which item to delete, this will be an async function
   try {  // try block handles any potential errors during the execution of the code
     const tagData = await Tag.destroy({  // destroy() deletes a specific tag identified by its primary key value
       where: {  // where option is used to specify which tag should be deleted based on its id
@@ -64,7 +64,7 @@ router.delete('/:id', async (req, res) => {
       },
     });
     res.status(200).json(tagData);  // returns json data if successful
-  } catch (err) {
+  } catch (err) {  // if an error occurs above, this will store the error in a variable named err
     console.error(err);  // log the error message
     res.status(500).json({error: err.message});  // return the error message in JSON format
   }
